f8e74e453ac3b454ecfe4c0f6de00631958ace6b
Tom Greuter <tom@Toms-MacBook-Pro.local>
2014-11-13 23:18:09 +0100

    Basic MVC setup

---
+++ example.js
@@ -0,0 +1,69 @@
+// Todo, keep away from global space
+
+var ToMvc = function() {
+    this.views  = [];
+    this.models = [];
+    this.events = [];
+
+    this.listen = function( event, fn ) {
+        base.events.push( {
+            view     : this.name,
+            event    : event,
+            callback : fn.bind( this )
+        });
+    }
+
+    this.broadcast = function ( event, data ) {
+        base.events.forEach( function( item ) {
+            if ( item.event === event ) {
+                item.callback( data );
+            }
+        });
+    }
+
+};
+
+ToMvc.prototype.registerView = function( name, el ) {
+    var view = new base.View( this, name, el );
+    this.views.push( view );
+    return view;
+}
+
+ToMvc.prototype.registerModel = function( name ) {
+    var model = new base.Model( this, name );
+    this.models.push( model );
+    return model;
+}
+
+
+ToMvc.prototype.View = function( base, name, el ) {
+    this.base = base;
+    this.name = name ? name : 'view';
+    this.el   = el;
+    this.listenTo = function( event ) {
+        base.listen.call( this, event, function( data ) {
+            console.info('view ' + this.name + ' called listener with data: ' + data );
+        } );
+    }
+}
+
+ToMvc.prototype.Model = function( base, name ) {
+    this.base = base;
+    this.name = name ? name : 'model';
+    this.broadcastTo = function( event ) {
+        base.broadcast.call( this, event, 'apples' );
+    }
+}
+
+
+var base  = new ToMvc();
+var viewA = base.registerView( 'A', 'element' );
+viewA.listenTo( 'eventA' );
+viewA.listenTo( 'eventB' );
+
+var viewB = base.registerView( 'B', 'element' );
+viewB.listenTo( 'eventB' );
+
+var modelA = base.registerModel( 'C' );
+modelA.broadcastTo ( 'eventB');
+
