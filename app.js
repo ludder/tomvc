3b8c64c677fd2618d0db9fe8e866785aa762def6
Tom Greuter <tom@toms-mbp.vistaprint.net>
2014-11-26 09:08:00 +0100

    Getting closer

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
-// Shape - superclass
-function Shape() {
-    this.x = 0;
-    this.y = 0;
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+// // Shape - superclass
+// function Shape() {
+//     this.x = 0;
+//     this.y = 0;
 
-    console.info('Shape called', this);
+//     console.info('Shape called', this);
 
-    // private function
-    function test( a ) {
-        console.info( 'in Shape = a', a );
-    }
-}
+//     // private function
+//     function test( a ) {
+//         console.info( 'in Shape = a', a );
+//     }
+// }
 
-// superclass method
-Shape.prototype.move = function( x, y ) {
-    this.x += x;
-    this.y += y;
-    console.info( 'Shape moved.' );
-};
+// // superclass method
+// Shape.prototype.move = function( x, y ) {
+//     this.x += x;
+//     this.y += y;
+//     console.info( 'Shape moved.' );
+// };
 
-// Rectangle - subclass
-function Rectangle( a ) {
-    console.info( 'nu zijn we hier', a );
-    Shape.call( this ); // call super constructor.
-}
+// // Rectangle - subclass
+// function Rectangle( a ) {
+//     console.info( 'nu zijn we hier', a );
+//     Shape.call( this ); // call super constructor.
+// }
 
-// subclass extends superclass
-Rectangle.prototype = Object.create( Shape.prototype );
-Rectangle.prototype.constructor = Rectangle;
+// // subclass extends superclass
+// Rectangle.prototype = Object.create( Shape.prototype );
+// Rectangle.prototype.constructor = Rectangle;
 
-var rect = new Rectangle( 'aapjes' );
+// var rect = new Rectangle( 'aapjes' );
 
-rect instanceof Rectangle; // true
-rect instanceof Shape; // true
+// rect instanceof Rectangle; // true
+// rect instanceof Shape; // true
 
-rect.move( 1, 1 ); // Outputs, 'Shape moved.'
+// rect.move( 1, 1 ); // Outputs, 'Shape moved.'
 
-Rectangle.prototype.doit = function( t ) {
-    console.info( 't =', t );
-}
+// Rectangle.prototype.doit = function( t ) {
+//     console.info( 't =', t );
+// }
 
-rect.doit( 'nu gaan' );
-// rect.test( 'nu gaan' );
+// rect.doit( 'nu gaan' );
+// // rect.test( 'nu gaan' );
 
 // //////////////////////
 // //////////////////////
@@ -49,19 +49,33 @@ rect.doit( 'nu gaan' );
 
 ( function() {
     var todolistController = Object.create( ToMvc.Controller );
-    var todolistView = Object.create( ToMvc.View );
-    var todolistModel = Object.create( ToMvc.Model );
+
+    // var todolistView = Object.create( ToMvc.View );
+    function TodolistView( name, el ) {
+        ToMvc.View.call( this, name, el ); // call super constructor.
+    }
+    TodolistView.prototype = Object.create( ToMvc.View.prototype );
+    TodolistView.prototype.constructor = TodolistView;
+
+    // var todolistModel = Object.create( ToMvc.Model );
+    function TodolistModel( name ) {
+        ToMvc.Model.call( this, name ); // call super constructor.
+    }
+    TodolistModel.prototype = Object.create( ToMvc.Model.prototype );
+    TodolistModel.prototype.constructor = TodolistModel;
+
     // TODO, create another app, to make sure that they do not interfere
 
     todolistController.init = function() {
         var todolist = document.querySelector( '#todolist' );
-        todolistView.init( 'todolist', '#todolist' );
-        todolistModel.init( 'todolist' );
+        var todolistView = new TodolistView( 'todolist', '#todolist' );
+        var todolistModel = new TodolistModel( 'todolist' );
 
         document.querySelector( '#add-todo button' ).addEventListener( 'click', todolistView.addTodo, false );
 
         var currentTodos = todolistModel.getCurrentTodos();
         todolistView.writeCurrentTodos( currentTodos );
+
 
         todolistModel.listenTo( 'todo:added', function( data ) {
             var key = window.localStorage.length + 1;
@@ -70,7 +84,7 @@ rect.doit( 'nu gaan' );
     }
 
     // model method
-    todolistModel.getCurrentTodos = function() {
+    TodolistModel.prototype.getCurrentTodos = function() {
         // function getCurrentTodos() {
         var list = [];
         for ( var key in localStorage ) {
@@ -80,14 +94,14 @@ rect.doit( 'nu gaan' );
         return list;
     };
 
-    todolistView.writeCurrentTodos = function( list ) {
+    TodolistView.prototype.writeCurrentTodos = function( list ) {
         list.forEach( function( item ) {
             todolistView.writeTodo( item );
         } );
     };
 
     // view method
-    todolistView.addTodo = function() {
+    TodolistView.prototype.addTodo = function() {
         var text = window.prompt( 'enter event' );
         if ( !text ) return;
 
@@ -101,7 +115,7 @@ rect.doit( 'nu gaan' );
     };
 
     // view method
-    todolistView.writeTodo = function( text ) {
+    TodolistView.prototype.writeTodo = function( text ) {
         var li = document.createElement( 'li' );
         li.textContent = text;
         todolist.appendChild( li );
--- tomvc.js
+++ tomvc.js
@@ -1,120 +1,155 @@
-var ToMvc = function() {
-    this.controller = function() {
-        // views
-        // models
-        // events
-        // other functions
-    }
-}
+( function() {
 
-// ToMvc.prototype.Controller = function() {
-var Controller = function() {
-    this.views = [];
-    this.models = [];
-    this.events = [];
-};
-// ToMvc.prototype.View = function( name, el ) {
-var View = function( name, el ) {
-    this.setName( name );
-    this.el = el;
-    console.info( 'con', controller );
-    controller.addView( name );
-};
-// ToMvc.prototype.Model = function() {
-var Model = function() {
-    this.name = 'model';
-};
+    var ToMvc = function() {
 
-var controller = new Controller(); // oef...
+        this.version = '0.1.0';
 
 
-/*
-    Controller
- */
-Controller.prototype.getViews = function() {
-    return this.views;
-};
-Controller.prototype.getModels = function() {
-    return this.models;
-};
-Controller.prototype.getEvents = function() {
-    return this.events;
-};
-Controller.prototype.addView = function( name ) {
-    this.views.push( name );
-};
-Controller.prototype.addModel = function( name ) {
-    this.models.push( name );
-};
-Controller.prototype.addEvent = function( eventname, callback ) {
-    this.events.push( {
-        'eventname': eventname,
-        'callback': callback
-    } );
-    // console.info('events:', ToMvc.Controller.events);
-};
-Controller.prototype.triggerEvent = function( eventname, data ) {
-    this.events.forEach( function( event ) {
-        if ( event.eventname === eventname ) {
-            event.callback( data );
-        }
-    } );
-};
+        this.Controller = function() {
+            return controller;
+        };
 
-/*
-    View
- */
-View.prototype.init = function( name, element ) {
-    this.setName( name );
-    this.el = element;
-    controller.addView( name ); // TODO
-};
-View.prototype.getName = function() {
-    return this.name;
-};
-View.prototype.setName = function( name ) {
-    this.name = name;
-};
-View.prototype.listenTo = function( event, callback ) {
-    controller.addEvent.call( this, event, callback );
-};
-View.prototype.broadcast = function( event, data ) {
-    controller.triggerEvent( event, data );
-};
+        this.View = function( name, el ) {
+            this.setName( name );
+            this.el = el;
+            // console.info( 'con', controller );
+            controller.addView( name );
+        };
+
+        this.Model = function( name ) {
+            this.name = 'model';
+        };
+    };
 
 
-/*
-    Model
- */
-Model.prototype.init = function( name ) {
-    this.setName( name );
-    Controller.addModel( this.name );
-};
-Model.prototype.getName = function() {
-    return this.name;
-};
-Model.prototype.setName = function( name ) {
-    this.name = name;
-    // TODO change name in controller model array
-};
-Model.prototype.listenTo = function( event, callback ) {
-    Controller.addEvent.call( this, event, callback );
-};
-Model.prototype.broadcast = function( event, data ) {
-    Controller.triggerEvent( event, data );
-};
+    // ToMvc.prototype.Controller = function() {
+    var Controller = function() {
+        this.views = [];
+        this.models = [];
+        this.events = [];
+    };
+    var View = function( name, el ) {
+        // ToMvc.prototype.View = function( name, el ) {
+        this.setName( name );
+        this.el = el;
+        controller.addView( name );
+    };
+    // var Model = function() {
+    // ToMvc.prototype.Model = function() {
+    //     this.name = 'model';
+    // };
 
+
+    /*
+        Controller
+    */
+    Controller.prototype.getViews = function() {
+        return this.views;
+    };
+    Controller.prototype.getModels = function() {
+        return this.models;
+    };
+    Controller.prototype.getEvents = function() {
+        return this.events;
+    };
+    Controller.prototype.addView = function( name ) {
+        this.views.push( name );
+    };
+    Controller.prototype.addModel = function( name ) {
+        this.models.push( name );
+    };
+    Controller.prototype.addEvent = function( eventname, callback ) {
+        this.events.push( {
+            'eventname': eventname,
+            'callback': callback
+        } );
+        // console.info('events:', ToMvc.Controller.events);
+    };
+    Controller.prototype.triggerEvent = function( eventname, data ) {
+        this.events.forEach( function( event ) {
+            if ( event.eventname === eventname ) {
+                event.callback( data );
+            }
+        } );
+    };
+
+    // We don't want the Controller to be accessible from outside
+    // Communications flow via View and Model
+    // These however, do not knwo of each others existince
+    // And communicate via events
+    var tomvc = new ToMvc;
+    var controller = new Controller;
+
+
+
+    /*
+        View
+     */
+    // Obsolete function?
+    tomvc.View.init = function( name, element ) {
+        console.info( 'huh', this );
+        this.setName( name );
+        this.el = element;
+        controller.addView( name ); // TODO
+    };
+    tomvc.View.prototype.getName = function() {
+        return this.name;
+    };
+    tomvc.View.prototype.setName = function( name ) {
+        this.name = name;
+    };
+    tomvc.View.prototype.listenTo = function( event, callback ) {
+        controller.addEvent.call( this, event, callback );
+    };
+    tomvc.View.prototype.broadcast = function( event, data ) {
+        controller.triggerEvent( event, data );
+    };
+
+
+    /*
+        Model
+     */
+    tomvc.Model.prototype.init = function( name ) {
+        this.setName( name );
+        Controller.addModel( this.name );
+    };
+    tomvc.Model.prototype.getName = function() {
+        return this.name;
+    };
+    tomvc.Model.prototype.setName = function( name ) {
+        this.name = name;
+        // TODO change name in controller model array
+    };
+    tomvc.Model.prototype.listenTo = function( event, callback ) {
+        controller.addEvent.call( this, event, callback );
+    };
+    tomvc.Model.prototype.broadcast = function( event, data ) {
+        Controller.triggerEvent( event, data );
+    };
+
+    // Make ToMvc globally available
+    window.ToMvc = tomvc;
+
+}() );
 
 /*
     Example
  */
 function SubView( name, el ) {
-    View.call( this, name, el ); // call super constructor.
+    ToMvc.View.call( this, name, el ); // call super constructor.
 }
 
 // subclass extends superclass
-SubView.prototype = Object.create( View.prototype );
+SubView.prototype = Object.create( ToMvc.View.prototype );
 SubView.prototype.constructor = SubView;
 
+// var c = ToMvc.Controller.addView();
+// console.info( 'c', c );
+
+
 var x = new SubView( 'aap' );
-console.info( 'views', x );
+console.info( 'views', x.getName() );
+var y = new SubView( 'neushoorn' );
+console.info( 'views', y.getName() );
+var z = new ToMvc.View( 'geit' );
+console.info( 'views', z.getName() );
